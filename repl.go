bd62d34037e41c32972698be918c643c0e9745c6
SethK99 <sgkloster@gmail.com>
2025-02-05 17:52:11 -0800

    added REPL logic

--- repl.go
+++ repl.go
@@ -1,12 +1,39 @@
 package main
 
 import (
-	
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+// REPL 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		
+		scanned := scanner.Scan()
+		if !scanned {
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(strings.TrimSpace(text)))
 	return words
 }
+
+
 	
