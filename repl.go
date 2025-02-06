b8235711b623642f32dc4e3922b8f78a32eb517b
SethK99 <sgkloster@gmail.com>
2025-02-05 18:22:30 -0800

    added help and exit commands

--- repl.go
+++ repl.go
@@ -7,33 +7,76 @@ import (
 	"fmt"
 )
 
-// REPL 
-func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
 
-	for {
-		fmt.Print("Pokedex > ")
-		
-		scanned := scanner.Scan()
-		if !scanned {
-			break
-		}
+var commands map[string]cliCommand
 
-		input := scanner.Text()
-		words := cleanInput(input)
-
-		if len(words) == 0 {
-			continue
-		}
-
-		fmt.Printf("Your command was: %s\n", words[0])
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(strings.TrimSpace(text)))
 	return words
 }
+
+// REPL 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		
+
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+		if cmd, exists := commands[commandName]; exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+
+
 
 
 	
